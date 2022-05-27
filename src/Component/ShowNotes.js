66248a97920be3cb4df89470973138bc056e1025
SabbirSami <smd71430@gmail.com>
2022-05-27 12:46:02 +0600

    added shownote component

---
+++ src/Component/ShowNotes.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShowNotes = ({ note }) => {
+    return (
+        <div>
+            <>
+                <h1>{note}</h1>
+            </>
+        </div>
+    );
+};
+
+export default ShowNotes;
