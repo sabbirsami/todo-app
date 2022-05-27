b186d9416bef86d1a983925e38ef11ffbf5925a1
SabbirSami <smd71430@gmail.com>
2022-05-27 11:29:32 +0600

    header and bg added

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./style.css";
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-center">To Do Notes</h1>
+        <div className="background" style={{ height: "100vh" }}>
+            <div className="header_text py-5">
+                <h1 className="text-center mt-lg-5 text-light">To Do Notes</h1>
+            </div>
         </div>
     );
 };
