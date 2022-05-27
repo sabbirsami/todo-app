6a5f329584f363bb06c91d8fafcf4bf4ab4b368d
SabbirSami <smd71430@gmail.com>
2022-05-27 12:44:02 +0600

    add state for get input value and fix input section design

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
@@ -6,15 +6,20 @@ import { MdOutlineStickyNote2 } from "react-icons/md";
 import { FiPlus } from "react-icons/fi";
 
 const Home = () => {
+    const [notes, setNotes] = useState([]);
+    console.log(notes);
+
     const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        setNotes(data.example);
+    };
     return (
         <div className="background" style={{ height: "100vh" }}>
             <div className="header_text text-light text-center py-5">
                 <h1 className=" mt-lg-5 ">
                     To Do Notes <MdOutlineStickyNote2 />
                 </h1>
-                <p>Add your list hare</p>
+                <p className="text-secondary">Add your list hare</p>
             </div>
             <div className="to_do_form mx-auto">
                 <form onSubmit={handleSubmit(onSubmit)}>
@@ -22,13 +27,13 @@ const Home = () => {
                         <div className=" px-2 py-3 rounded-3 d-inline">
                             <div className=" bg-success d-inline py-2 rounded-3">
                                 <input
-                                    className="border-0 me-3 ps-3 fs-5 bg-light rounded-3"
+                                    className="border-0 me-3 ps-3 pb-1 fs-5 bg-light rounded-3"
                                     placeholder="Write your note hare..."
                                     {...register("example")}
                                 />
                                 <button
                                     type="submit"
-                                    className="border-0 pb-1  bg-light rounded-3 "
+                                    className="border-0 mb-2 bg-white shadow rounded-3 "
                                 >
                                     <FiPlus></FiPlus>
                                 </button>
