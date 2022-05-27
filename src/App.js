24bedb73ce01b4d9ffb22b19ebd0fcf2e416ce85
SabbirSami <smd71430@gmail.com>
2022-05-27 22:44:41 +0600

    login component added and set up react routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Component/Home";
 import { Toaster } from "react-hot-toast";
 import HeaderNav from "./Component/HeaderNav";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Component/Login";
 
 function App() {
     return (
         <div>
             <HeaderNav></HeaderNav>
             <Home></Home>
+            <Routes>
+                <Route path="/login" element={<Login></Login>}></Route>
+            </Routes>
             <Toaster position="top-center" reverseOrder={false} />
         </div>
     );
