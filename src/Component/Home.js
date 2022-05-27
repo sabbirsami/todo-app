d8d6f752a3eea0db1582432f360781cc4e2b3b61
SabbirSami <smd71430@gmail.com>
2022-05-27 11:32:38 +0600

    react hook form added

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
     return (
         <div className="background" style={{ height: "100vh" }}>
             <div className="header_text py-5">
                 <h1 className="text-center mt-lg-5 text-light">To Do Notes</h1>
+            </div>
+            <div className="to_do_form">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input defaultValue="test" {...register("example")} />
+                    <input type="submit" />
+                </form>
             </div>
         </div>
     );
